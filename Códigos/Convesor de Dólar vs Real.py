82504d57087ce4e0f9c81bc3cc3af47fe5992b9f
Gabriel Sales <73443927+gabrielmgsales@users.noreply.github.com>
2023-09-22 12:54:47 -0300

    Update Convesor de Dólar vs Real.py

--- Códigos/Convesor de Dólar vs Real.py
+++ Códigos/Convesor de Dólar vs Real.py
@@ -1,4 +1,4 @@
 real = float(input('Quanto deseja converter ? R$').replace(',', '.'))
 dolar = real / 4.86
 
-print ('Com R${:.2f} você poderá comprar U$${:.2f}'.format(real, (real/4.86)))
+print ('Com R${:.2f} você poderá comprar U$${:.2f}'.format(real, dolar))
